ff7d5ede22ee172965729f507b98ecbb58d7cab8
elvmariga <elvmariga@gmail.com>
2023-02-14 12:49:50 +0300

    modified:   src/components/inventory/Products.jsx

--- src/components/inventory/Products.jsx
+++ src/components/inventory/Products.jsx
@@ -1,42 +1,92 @@
-import React, { Component } from "react";
+import React, { useEffect, useRef } from "react";
 import $ from "jquery";
 import "datatables.net";
+import "datatables.net-dt/css/jquery.dataTables.css";
 
-class Product extends Component {
-  componentDidMount() {
-    $(this.refs.myTable).DataTable();
-  }
+const Product = () => {
+  const tableRef = useRef();
 
-  render() {
-    const data = [
-      { name: "John", age: 25, city: "New York" },
-      { name: "Jane", age: 30, city: "Los Angeles" },
-      { name: "Bob", age: 40, city: "Chicago" },
-    ];
+  useEffect(() => {
+    $(tableRef.current).DataTable();
+  }, []);
 
-    const rows = data.map((row, index) => {
-      return (
-        <tr key={index}>
-          <td>{row.name}</td>
-          <td>{row.age}</td>
-          <td>{row.city}</td>
-        </tr>
-      );
-    });
+  const data = [
+    {
+      product: "Red Bull",
+      buyingPrice: 200,
+      quantity: "12 packets",
+      thresholdValue: "12 packets",
+      expiryDate: "11/02/2023",
+      availability: "in stock",
+    },
+    {
+      product: "Red Bull",
+      buyingPrice: 200,
+      quantity: "12 packets",
+      thresholdValue: "12 packets",
+      expiryDate: "11/02/2023",
+      availability: "in stock",
+    },
+    {
+      product: "Red Bull",
+      buyingPrice: 200,
+      quantity: "12 packets",
+      thresholdValue: "12 packets",
+      expiryDate: "11/02/2023",
+      availability: "in stock",
+    },
+  ];
 
+  const rows = data.map((row, index) => {
     return (
-      <table ref="myTable">
+      <tr key={index} className="border-b">
+        <td className="text-left border-b">{row.product}</td>
+        <td className="text-left border-b">{row.buyingPrice}</td>
+        <td className="text-left border-b">{row.quantity}</td>
+        <td className="text-left border-b">{row.thresholdValue}</td>
+        <td className="text-left border-b">{row.expiryDate}</td>
+        <td className="text-left border-b">{row.availability}</td>
+      </tr>
+    );
+  });
+
+  return (
+    <div className="mt-16px shadow-xl bg-white-50 p-4">
+      <table ref={tableRef} className="table-auto w-full ">
         <thead>
           <tr>
-            <th>Name</th>
-            <th>Age</th>
-            <th>City</th>
+            <td colSpan="6" className="w-full">
+              <div className="flex justify-between items-center px-4 py-2 ">
+                <div>
+                  <h1 className="text-xl font-bold">Product</h1>
+                </div>
+                <div>
+                  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    Button 1
+                  </button>
+                  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2">
+                    Button 2
+                  </button>
+                  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2">
+                    Button 3
+                  </button>
+                </div>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <th className="text-center">Product</th>
+            <th className="text-center">Buying Price</th>
+            <th className="text-center">Quantity</th>
+            <th className="text-center">Threshold Value</th>
+            <th className="text-center">Expiry Date</th>
+            <th className="text-center">Availability</th>
           </tr>
         </thead>
         <tbody>{rows}</tbody>
       </table>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Product;
