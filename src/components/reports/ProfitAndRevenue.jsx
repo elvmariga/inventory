2280a2b4765c5e1b5fda7f510a6822180c489e97
kiptanuiBoaz <boaserem022@gmail.com>
2023-02-16 15:53:21 +0300

    added best seller section

--- src/components/reports/ProfitAndRevenue.jsx
+++ src/components/reports/ProfitAndRevenue.jsx
@@ -1,38 +1,38 @@
 import React from 'react';
 import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend,
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
 } from 'chart.js';
 import { Line } from 'react-chartjs-2';
 
 
 ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
 );
 
 export const options = {
-  responsive: true,
-  plugins: {
-    legend: {
-      position: 'top',
+    responsive: true,
+    plugins: {
+        legend: {
+            position: 'bottom',
+        },
+        title: {
+            display: true,
+            text: 'Profit and revenue',
+        },
     },
-    title: {
-      display: true,
-      text: 'Chart.js Line Chart',
-    },
-  },
 };
 
 
@@ -41,31 +41,30 @@ export const options = {
 const data = {
     labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
     datasets: [
-      {
-        label: 'My First Dataset',
-        data: [65, 59, 80, 81, 56, 55, 40],
-        fill: false,
-        borderColor: 'rgb(75, 192, 192)',
-        tension: 0.1
-      },
+        {
+            label: 'revenue',
+            data: [65, 59, 80, 81, 56, 55, 40],
+            fill: false,
+            borderColor: 'rgb(75, 192, 192)',
+            tension: 0.1
+        },
 
-      {
-        label: 'My second Dataset',
-        data: [35, 69, 70, 91, 46, 65, 50],
-        fill: false,
-        borderColor: 'rgb(255, 99, 132)',
-        tension: 0.1
-      },
+        {
+            label: 'profits',
+            data: [35, 69, 70, 91, 46, 65, 50],
+            fill: false,
+            borderColor: 'rgb(255, 99, 132)',
+            tension: 0.1
+        },
 
 
-      
+
     ]
-  };
-  
+};
 
-const  ProfitAndRevenue = () => {
 
-  return <Line options={options} data={data} />;
+const ProfitAndRevenue = () => {
+    return <Line options={options} data={data} />;
 }
 
 
